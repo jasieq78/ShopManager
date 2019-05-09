f523a5dafde68e9210a0dc40bc9ebb6bafa177d8
gitINPG <slis@student.agh.edu.pl>
2019-05-09 09:28:14 +0200

    Praca nad porówynywaniem cen

--- purchase/include/purchase.hpp
+++ purchase/include/purchase.hpp
@@ -5,4 +5,17 @@
 #ifndef PURCHASE_PURCHASE_HPP
 #define PURCHASE_PURCHASE_HPP
 
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <string>
+
+std::vector<std::string> catalogue() {}
+
+bool wczytajPlik( std::string nazwaPliku ) {} 
+//nazwa pliku = ka¿dy element vectora catalogue
+
+
+
 #endif //PURCHASE_PURCHASE_HPP
--- purchase/src/purchase.cpp
+++ purchase/src/purchase.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "purchase.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <string>
+
+
+std::vector<std::string> catalogue() {
+    std::cout << "Wprowadzanie katalogów hurtowni do bazy" << std::endl;
+    std::cout
+            << "Aby dodać hurtownie należy wpisać nazwe jej katalogu razem z rozszerzeniem w jakim jest zapisany np .txt"
+            << std::endl;
+    std::cout << "Ile katalogow ma byc dodane?" << std::endl;
+    int n, i;
+    std::cin >> n;
+    std::vector<std::string> catalogue_list;
+    std::string tmp;
+    for (i = 0; i < n; i++) {
+        std::getline(std::cin, tmp);
+        catalogue_list.push_back(tmp)
+    }
+    return catalogue_list;
+}
+//for( size_t i = 0; i < dane.size(); i++ )
+//printf( "%d, ", dane[ i ] );
+bool wczytajPlik( std::string nazwaPliku )
+{
+    std::ifstream plik;
+    plik.open( nazwaPliku.c_str() );
+    if( !plik.good() )
+        return false;
+
+    while( true )
+    {
+
+            // tutaj bedzie przeszukiwanie
+        else
+            break;
+
+    }
+    return true;
+}
